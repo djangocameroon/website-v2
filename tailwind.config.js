766d8b976b6f8d0c83228c76ccb8a839c6fb9ca7
mankaa che <mankaache10@gmail.com>
2023-12-28 10:50:09 +0100

    added base colors to the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': 'rgba(16, 62, 46, 1)',
+        'secondary': 'rgba(66, 133, 244, 1)',
+        'dark-primary': '#C6EADE',
+      },
+    },
   },
   plugins: [],
-}
+};
 
