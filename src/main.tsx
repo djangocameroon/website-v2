82a074013d0a250ebbd55b2a2f9f1b0a64dd4fe7
mankaa che <mankaache10@gmail.com>
2023-12-28 16:13:27 +0100

    added root routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import {store} from './redux/store.ts';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
+import PageNotFound from './components/not-found/Page-notfound.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageNotFound/>
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-)
+);
