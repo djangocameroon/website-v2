983f530c861550bc5eeb7be2da59c24b59e06364
mankaa che <mankaache10@gmail.com>
2024-01-26 20:31:35 +0100

    set up other pages

---
+++ src/pages/Project.tsx
@@ -0,0 +1,12 @@
+import ProjectsSection from '../components/pages/Home/ProjectsSection'
+
+
+const Project = () => {
+  return (
+    <div id='project'>
+        <ProjectsSection/>
+    </div>
+  )
+}
+
+export default Project
