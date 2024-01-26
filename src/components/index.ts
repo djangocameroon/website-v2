ab4b37da759e82b1dbe5d49d1c4f9dcc5a26cca0
mankaa che <mankaache10@gmail.com>
2024-01-26 12:01:33 +0100

    added exports for components

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import AvatarUsers from './AvatarUsers'
+import ProjectCard from './ProjectCard'
+import ToggleSwitch from './ToggleSwitch'
+import PageNotFound from './PageNotFound'
+import EventCard from './EventCard'
+
+export {
+    AvatarUsers,
+    ProjectCard,
+    ToggleSwitch,
+    PageNotFound,
+    EventCard
+}
