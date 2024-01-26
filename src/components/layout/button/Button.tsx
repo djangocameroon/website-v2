93651c91f705b25986520568904f47296e7cc68d
mankaa che <mankaache10@gmail.com>
2024-01-26 20:27:51 +0100

    added a button comp

---
+++ src/components/layout/button/Button.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+interface ButtonProps {
+  backgroundColor?: string;
+  children: ReactNode;
+  onClick?: () => void;
+  outline: boolean;
+}
+
+const Button = ({ outline, children,backgroundColor }: ButtonProps) => {
+  return (
+    <button
+      className={` ${
+        outline === true ? 'btn-outline' : ''
+      } capitalize  py-2 px-5 rounded-lg  text-white text-lg font-semibold ${backgroundColor} `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button
