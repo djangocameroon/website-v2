4a19b0fed2da643af122171f37307e58cd9d6452
mankaa che <mankaache10@gmail.com>
2024-01-26 20:25:35 +0100

    added the page not found comp

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,14 @@
+
+const PageNotFound = () => {
+
+
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      
+    </div>
+  );
+};
+
+export default PageNotFound;
