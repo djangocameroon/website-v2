0567fa8357cd2838d2c0b42466e2e69089b95646
mankaa che <mankaache10@gmail.com>
2024-01-26 12:00:41 +0100

    added image exports

---
+++ src/assets/images/home/home.ts
@@ -0,0 +1,21 @@
+import arrow from '/images/home/arrow.svg';
+import bird from '/images/home/bird.svg';
+import channel from '/images/home/channel.png';
+import connect from '/images/home/connect.svg';
+import curlArrow from '/images/home/curl-arrow.svg';
+import getStarted from '/images/home/get-started.svg';
+import greenBackBg from '/images/home/greenbackbg.svg';
+import greenBg from '/images/home/greenbg.svg';
+import Group from '/images/home/Group 131.svg';
+import Logo from '/images/home/logo-girls.svg';
+import mankaa from '/images/home/mankaa.svg';
+import map from '/images/home/map.svg';
+import projectBg from '/images/home/projectbg.svg';
+import spaceShip from '/images/home/space-ship.svg';
+import whyJoin from '/images/home/whyjoin.svg';
+import youtubeThumbnail from '/images/home/youtube-thumbnail.svg';
+
+
+
+
+export { arrow, bird,connect, curlArrow, getStarted, greenBackBg, greenBg, Group, Logo, mankaa, map, projectBg, spaceShip, whyJoin, youtubeThumbnail, channel };
