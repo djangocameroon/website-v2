49061edcea8ea89ff2471b110c9ca764779c7e30
mankaa che <mankaache10@gmail.com>
2024-02-02 14:11:56 +0100

    adjusted navbar and footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -7,8 +7,8 @@ import { LanguageSwitcher } from '../..';
 
 const Footer = () => {
   return (
-    <div className='mt-20 flex flex-col md:flex-row gap-2 py-4 justify-between items-center w-full lg:w-[85%] lg:mx-auto border-t-2 border-gray-800'>
-      <div className='font-bold text-2xl cursor-pointer flex items-start gap-1'>
+    <div className=' flex bg-transparent flex-col md:flex-row gap-2 py-4 justify-between items-center w-full lg:w-[85%] lg:mx-auto border-t-2 border-gray-800'>
+      <div className='font-bold bg-transparent text-2xl cursor-pointer flex items-start gap-1'>
         <img src={HomeImages.Logo} alt='logo image' className='w-36 h-16' />
       </div>
       <div className='text-lg md:text-base text-center md:text-left'>
--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -4,12 +4,16 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { HomeImages } from '../../../assets';
 import { Button } from '../../layout';
 import { ToggleSwitch } from '../../../components';
+import { NavLink, useLocation } from 'react-router-dom';
+
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const {pathname} = useLocation()
+  const isAuthRoute = pathname.includes('/auth')
 
   return (
-    <div className='w-full z-[7000] h-full fixed top-0 left-0 '>
+    <div className={`w-[99.2%] ${isAuthRoute ? 'hidden' : 'block'}`}>
       <div className='md:flex  bg-white items-center justify-between bg-transparent py-4 md:px-10 px-7'>
         {/* logo section */}
         <div className='font-bold text-2xl cursor-pointer flex items-start gap-1'>
@@ -28,60 +32,60 @@ const Navbar = () => {
             open ? 'left-0' : 'left-[-990px]'
           }`}
         >
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#home'
-              className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
+          <li className='md:ml-[0.6rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='/'
+              className={`text-gray-800 text-xl capitalize  hover:text-blue-400 duration-500`}
             >
               Home
-            </a>
+            </NavLink>
           </li>
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#about'
+          <li className='md:ml-[0.5rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='#about'
               className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
             >
               About
-            </a>
+            </NavLink>
           </li>
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#event'
+          <li className='md:ml-[0.6rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='#event'
               className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
             >
               Event
-            </a>
+            </NavLink>
           </li>
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#project'
+          <li className='md:ml-[0.6rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='#project'
               className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
             >
               Project
-            </a>
+            </NavLink>
           </li>
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#contact'
+          <li className='md:ml-[0.6rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='#contact'
               className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
             >
               Contact
-            </a>
+            </NavLink>
           </li>
-          <li className='md:ml-8 md:my-0 my-7 font-semibold '>
-            <a
-              href='#chapter'
+          <li className='md:ml-[0.6rem] lg:ml-8 md:my-0 my-7 font-semibold '>
+            <NavLink
+              to='#chapter'
               className='text-gray-800 text-xl capitalize hover:text-blue-400 duration-500'
             >
               Chapter
-            </a>
+            </NavLink>
           </li>
           <li className=' flex items-start justify-between flex-col gap-3 md:hidden '>
             <Button
               outline={false}
               backgroundColor='bg-primary whitespace-nowrap'
             >
-              Sign Up
+              <NavLink to='/auth/login'>Login</NavLink>
             </Button>
             <div className='mt-1'>
               <ToggleSwitch />
@@ -90,7 +94,7 @@ const Navbar = () => {
         </ul>
         <div className='mx-4 items-center justify-between gap-3 hidden md:flex'>
           <Button outline={false} backgroundColor='bg-primary'>
-            Sign Up
+            <NavLink to='/auth/login'>Login</NavLink>
           </Button>
           <div className='mt-1'>
             <ToggleSwitch />
