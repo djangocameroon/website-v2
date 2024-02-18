781baa96cd16f8afd93567452bc409125e9e2a1a
mankaache <mankaache10@gmail.com>
2024-02-18 12:44:57 +0100

    removed footer on auth pages

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -3,11 +3,16 @@ import { VscGithubAlt } from 'react-icons/vsc';
 import { CiTwitter } from 'react-icons/ci';
 import { FiLinkedin, FiYoutube } from 'react-icons/fi';
 import { LanguageSwitcher } from '../..';
+import { useLocation } from 'react-router-dom';
 
 
-const Footer = () => {
+const Footer = () => { 
+
+    const { pathname } = useLocation();
+    const isAuthRoute = pathname.includes('/auth');
+
   return (
-    <div className=' flex bg-transparent flex-col md:flex-row gap-2 py-4 justify-between items-center w-full lg:w-[85%] lg:mx-auto border-t-2 border-gray-800'>
+    <div className={`${isAuthRoute ? 'hidden' : 'block'} flex bg-transparent flex-col md:flex-row gap-2 py-4 justify-between items-center w-full lg:w-[85%] lg:mx-auto border-t-2 border-gray-800`}>
       <div className='font-bold bg-transparent text-2xl cursor-pointer flex items-start gap-1'>
         <img src={HomeImages.Logo} alt='logo image' className='w-36 h-16' />
       </div>
