dca0ff6034d488e5dadded7f992abf5bb680d0e8
mankaache <mankaache10@gmail.com>
2024-02-18 12:44:17 +0100

    added the image links of the about page to assets

---
+++ src/assets/images/about/about.ts
@@ -0,0 +1,15 @@
+import rocket from '/images/about/rocket.png'
+import house1 from '/images/about/national-museum.png'
+import house2 from '/images/about/house2.png'
+import landscape1 from '/images/about/landscape.png'
+import landscape2 from '/images/about/landscape2.png'
+import peopleTogether from '/images/about/people-together.png'
+
+export {
+    rocket,
+    house1,
+    house2,
+    landscape1,
+    landscape2,
+    peopleTogether
+}
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export * as HomeImages from './home/home'
 export * as AuthImages from './auth/auth'
 export * as Avatars from './avatars/avatars'
+export * as AboutImages from './about/about'
