fa1f11c9d0c6f96181c4a0f41d87ef6e92a2e78c
mankaache <mankaache10@gmail.com>
2024-02-18 12:46:33 +0100

    added the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,25 @@
-
+import {
+  AboutHeader,
+  JoinUsNow,
+  MissionVission,
+  Organisers,
+  WhereITStarted,
+} from '../components/pages/About-Page-Components';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className='relative'>
+      <AboutHeader/>
+      <div className='w-full md:w-[85%] mx-auto'>
+        <div className='my-10 w-full ml-auto flex flex-col gap-10'>
+            <WhereITStarted />
+            <MissionVission />
+            <Organisers />
+        </div>
+        <JoinUsNow />
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
