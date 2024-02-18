6f37ad9e69b64d5a6e9fc5abc4b85e4b937502e0
mankaache <mankaache10@gmail.com>
2024-02-18 12:44:45 +0100

    reduced the size of the card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,12 +5,12 @@ import { FaArrowRightLong } from 'react-icons/fa6';
 
 const ProjectCard = () => {
   return (
-    <div className='cursor-pointer bg-white px-4 py-3 max-w-[24em] border border-gray-400 rounded-lg shadow-xl'>
+    <div className='cursor-pointer bg-white px-4 py-2 max-w-[20em] border border-gray-400 rounded-lg shadow-xl'>
       <div>
         <img src={HomeImages.projectBg} alt='' className='rounded-lg' />
       </div>
       <div>
-        <p className='text-primary text-lg mt-3 mb-3'>
+        <p className='text-primary text-base mt-3 mb-3'>
           Doing something more interesting and funny with Django rather than
           just python manage.py runserver
         </p>
@@ -22,7 +22,7 @@ const ProjectCard = () => {
             </div>
           </Badge>
         </div>
-        <div className='flex justify-start items-center gap-3 mt-12'>
+        <div className='flex justify-start items-center gap-3 mt-10'>
           <p className='text-text-color/80 text-sm font-bold'>Read More</p>
           <FaArrowRightLong className='text-sm text-text-color' />
         </div>
