0eace825c8af5a864ce7670ae8fb2d452c2e72de
mankaache <mankaache10@gmail.com>
2024-02-20 12:20:45 +0100

    added the organisers images

--- src/assets/images/about/about.ts
+++ src/assets/images/about/about.ts
@@ -4,6 +4,17 @@ import house2 from '/images/about/house2.png'
 import landscape1 from '/images/about/landscape.png'
 import landscape2 from '/images/about/landscape2.png'
 import peopleTogether from '/images/about/people-together.png'
+import velda from '/images/about/velda.jpg'
+import etiane from '/images/about/etiane.jpg'
+import edmond from '/images/about/edmond.jpg'
+import christian from '/images/about/christian.jpg'
+import olga from '/images/about/olga.jpg'
+import steve from '/images/about/steve.jpg'
+import joel from '/images/about/joel.jpg'
+import leo from '/images/about/leo.jpg'
+import manka from '/images/about/manka.jpg'
+import ariane from '/images/about/ariane.jpg'
+import hawa from '/images/about/hawa.jpg'
 
 export {
     rocket,
@@ -11,5 +22,16 @@ export {
     house2,
     landscape1,
     landscape2,
-    peopleTogether
+    peopleTogether,
+    velda,
+    edmond,etiane,
+    olga,
+    christian,
+    steve,
+    leo,
+    joel,
+    manka,
+    ariane,
+    hawa
+    
 }
