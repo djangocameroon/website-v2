0de31fa2deaf0a8d0d9b9bb5ced3c23cb1a313bb
Steve Yonkeu <yokwejuste@yahoo.com>
2025-12-06 03:42:48 +0100

    fix: update server and preview configuration in vite config
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: true,
   },
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: ['all'],
+    strictPort: true,
+    allowedHosts: true,
   },
 })
